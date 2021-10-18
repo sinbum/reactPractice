e15c568eb6337c598a4c9ee90fa0f433fdacb961
sinbum <sinbum@kakao.com>
2021-10-18 16:04:47 +0900

    too list 와 form 작성

---
+++ src/components/TodoItemList.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class TodoItemList extends Component {
+    render() {
+        const {todos, myToggle, myRemove } = this.props;
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default TodoItemList;
