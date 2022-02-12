cf4cca0f53a67cb14a77dab1213ce7f1d022d500
sinbum Choi <sinbum@kakao.com>
2022-02-12 16:04:00 +0900

    useState사용으로 버튼누르면 나이 값 올리기

--- src/App.js
+++ src/App.js
@@ -8,9 +8,9 @@ function App() {
      
     
       {/* 컴포넌트를 만들고 재사용 할경우 각각의 컴포넌트의 상태값들은 같이 변하지 않음을 인지하고 있어야한다. */}
-       <Hello></Hello>      
-       <Hello></Hello>
-       <Hello></Hello>
+       <Hello age={10}></Hello>      
+       <Hello age={20}></Hello>
+       <Hello age={30}></Hello>
 
 
        
--- src/practice/Hello.js
+++ src/practice/Hello.js
@@ -2,17 +2,19 @@ import { useState } from "react";
 
 
 
-export default function Hello() {
+export default function Hello(props) {
+    //console.log(props);
     const[name, setName] = useState('Mike');
-
+    const[age, setAge] = useState(props.age);
 
     return(
         <div>
             <h1>state</h1>
-            <h2 id="name">{name}</h2>
+            <h2 id="name">{name}({age})</h2>
            <button onClick={()=>{
                  const newName = name === 'Mike' ? 'Jain' : 'Mike';
                  setName(newName);
+                 setAge(age+1);
            }}>Change</button>
         
         </div>
