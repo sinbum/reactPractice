57fb378d888deeef99e31f183137c9702a4503c1
sinbum Choi <sinbum@kakao.com>
2022-02-12 13:38:44 +0900

    .

--- src/App.js
+++ src/App.js
@@ -6,11 +6,10 @@ function App() {
   return (
     <div className="App">
      
-       {/* <Section/> */}
-
+    
+      {/* 컴포넌트를 만들고 재사용 할경우 각각의 컴포넌트의 상태값들은 같이 변하지 않음을 인지하고 있어야한다. */}
+       <Hello></Hello>      
        <Hello></Hello>
-       <Hello></Hello>
-
        <Hello></Hello>
 
 
