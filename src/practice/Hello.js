bc44bbaae5fe0287102d5880909bc55ae4f66abf
sinbum Choi <sinbum@kakao.com>
2022-02-12 13:34:26 +0900

    state, useState __함수를 간략하게 콜백으로 사용한 예.

--- src/practice/Hello.js
+++ src/practice/Hello.js
@@ -5,16 +5,15 @@ import { useState } from "react";
 export default function Hello() {
     const[name, setName] = useState('Mike');
 
-    function changeName(){
-        const newName = name === 'Mike' ? 'Jain' : 'Mike';
-        setName(newName);
-    }
 
     return(
         <div>
             <h1>state</h1>
             <h2 id="name">{name}</h2>
-           <button onClick={changeName}>Change</button>
+           <button onClick={()=>{
+                 const newName = name === 'Mike' ? 'Jain' : 'Mike';
+                 setName(newName);
+           }}>Change</button>
         
         </div>
 
