3cb7d0146e42b96bb7de0f4143a2ebf5413f5030
sinbum Choi <sinbum@kakao.com>
2022-02-12 13:33:28 +0900

    state, useState 사용

--- src/practice/Hello.js
+++ src/practice/Hello.js
@@ -1,28 +1,23 @@
+import { useState } from "react";
+
+
+
 export default function Hello() {
-    function showName(){
-        console.log ('mike');
-    }
+    const[name, setName] = useState('Mike');
 
-    function showAge(age) {
-        console.log(age);
-    }
-
-    function showText(txt) {
-        console.log(txt)
+    function changeName(){
+        const newName = name === 'Mike' ? 'Jain' : 'Mike';
+        setName(newName);
     }
 
     return(
         <div>
             <h1>state</h1>
-            <h2>컴포넌트의 속성값            </h2>
-            <button onClick={showName}> Show name </button>       
-            <button onClick={()=>{showAge(10)}}> show Age</button>
-            <input type="text" onChange={e=>{const txt = e.target.value; showText(txt)}} />
-        
-        
+            <h2 id="name">{name}</h2>
+           <button onClick={changeName}>Change</button>
         
         </div>
 
-        )
+        );
 
 }
