6ce1b5d0a28dc349698d9ca179720597f67f99f6
sinbum Choi <sinbum@kakao.com>
2022-02-14 21:08:54 +0900

    useEffect 사용

--- src/practice/Day.js
+++ src/practice/Day.js
@@ -1,6 +1,7 @@
 // 화면상의 리스트에서 특정 날짜를 클릭했을때 단어들이 나오는 페이지.
 import dummy from "./data/data.json";
 import { useParams } from "react-router-dom";
+import Word from "./Word";
 
 export default function Day() {
 
@@ -24,14 +25,7 @@ const wordList = dummy.words.filter(word => (word.day === Number(day)));
         <table>
             <tbody>
                 {wordList.map(word => (
-                    <tr key = {word.id}>
-                        <td>
-                            {word.eng}
-                        </td>
-                        <td>
-                            {word.kor}
-                        </td>
-                    </tr>
+                  <Word word={word} key={word.id}></Word>
                 ))}
             </tbody>
         </table>
--- src/practice/DayList.js
+++ src/practice/DayList.js
@@ -1,26 +1,48 @@
+import {useEffect, useState} from "react";
 import {Link} from "react-router-dom"
 
-import dummy from "./data/data.json"
-
 export default function DayList() {
-
+    const [days, setDays] = useState([]);
     //console.log(dummy)
+    const [count, setCount] = useState(0);
 
-    return <ul className="list_day">
+    function onclick(){
+        setCount(count + 1);
+    }
+
+    function onclick2(){
+        setDays([
+            ...days,
+            {
+                id: Math.random(),
+                day: 1,
+            }]
+        );
+    }
+
+    useEffect(() => {
+        console.log('change');
+    }, []); //렌더링 직후 딱 한번만 실행하는것은 빈배열을 전달하면 된다.
+
+
+
+    return (<>
+    <ul className="list_day">
         {
-            dummy
-                .days
-                .map(row => (
-                    // html에서링크를 사요하려면 a태그를 사용하지만 리액트 라우터는 link라는 것을 사용한다.
+            days.map(row => (
+                // html에서링크를 사요하려면 a태그를 사용하지만 리액트 라우터는 link라는 것을 사용한다.
 
-                    <li key={row.id}>
-                        {/* <Link to={`/day/${day.day}`}>Day {day.day}</Link> */}
+                <li key={row.id}>
+                    {/* <Link to={`/day/${day.day}`}>Day {day.day}</Link> */}
 
-                        {/* 쿼테이션을 사용하지말 ` 1옆에있는 이 따옴표를 사용해야 한다. */}
-                        <Link to={`/day/${row.day}`}>Day {row.day}</Link>
-                    </li>
-                ))
-
+                    {/* 쿼테이션을 사용하지말 ` 1옆에있는 이 따옴표를 사용해야 한다. */}
+                    <Link to={`/day/${row.day}`}>Day {row.day}</Link>
+                </li>
+            ))
         }
     </ul>
+    <button onClick={onclick}>{count}</button>
+    <button onClick={onclick2}>Day Change</button>
+
+    </>)
 }
